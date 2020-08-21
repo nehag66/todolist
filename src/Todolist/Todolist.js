fbab072915eca14a317f214e81b8ff822f8d088a
Neha Goel <nehagoel66@gmail.com>
2020-08-21 19:35:05 +0530

    todolist added

---
+++ src/Todolist/Todolist.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+
+import classes from './Todolist.css';
+
+class Todolist extends Component {
+    
+    state = {
+        value: '',
+        items: []
+    }
+
+    // onClickHandler = (event) => {
+    //     //console.log('clicked');
+    //     //console.log(this.state.value);
+        
+    //     const item = document.getElementById("input").value;
+    //     console.log(item);   
+    // }
+    
+    itemHandler = (event) => {
+        event.preventDefault();
+        this.setState({value: event.target.value});    
+    }
+
+    addItem = () => {
+        let items = this.state.items;
+        items.push(this.state.value);
+        this.setState({ items });
+    }
+
+    listItems() {
+        let items = this.state.items;
+        return (
+            <ul>
+                {
+                    items.map((val, index) => {
+                        return (
+                            <li key={index} >
+                                {val}
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        );
+    }  
+
+    render() {    
+        return (     
+            <div className={classes.Main}>
+                    <input 
+                        className={classes.Input}
+                        placeholder="Enter Task"
+                        id="input"
+                        value={this.state.value}
+                        onChange={(event) => this.itemHandler(event)} />
+                    <button 
+                        type="submit"
+                        className={classes.Button}
+                        onClick={() => this.addItem()}>
+                            ADD ITEM
+                    </button>
+                    {this.listItems()}
+                   
+            </div>
+        );
+    }
+}
+
+export default Todolist;
