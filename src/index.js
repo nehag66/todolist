629aa980e919805f988679bc9d6933c1905d4f6f
Rishabh jain <rishabhjain.rdps@gmail.com>
2020-10-01 12:19:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// importing todolist
 import Todolist from './Todolist/Todolist';
 
 ReactDOM.render(<Todolist />, document.getElementById('root'));
